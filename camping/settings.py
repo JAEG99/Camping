7659beebb2578b94fbedce19de7ae7d46bd0497c
JAEG <j.a.e.gilliland@outlook.com>
2024-01-13 21:54:26 +0000

    Remove secret key and set Debug

--- camping/settings.py
+++ camping/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w^027c)c9mjh!xyf))#1ux0pt8%=jz$d!d*55z#*4^f^y2m@q1'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['camping.herokuapp.com', 'localhost']
 
