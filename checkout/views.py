177b210241cd11916b59bfb4135469dd90817989
JAEG <j.a.e.gilliland@outlook.com>
2024-01-13 13:44:02 +0000

    Tests stripe

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51OY729DwSqJ3sD1egUfwOXfpnGTKPDcxaYDQqUycbJlYDWhSPdhmTs8t9euVbepppXuzIqS3FzvQKdvxLYJS6tW800y7avFK3j',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
